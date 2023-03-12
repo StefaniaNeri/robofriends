3fb8dd5a9711277fc4c99b2524e23944db714681
StefaniaNeri <stefania.neri.92@gmail.com>
2023-03-12 12:12:14 +0100

    Changing main to default

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn Reacti
+          Learn Reacting
         </a>
       </header>
     </div>
