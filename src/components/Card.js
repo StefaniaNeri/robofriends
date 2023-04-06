b779021eebd6e44fedc3a4ed9d2ca301a90f5f7b
StefaniaNeri <117984737+StefaniaNeri@users.noreply.github.com>
2023-04-06 20:44:09 +0200

    Update Card.js

--- src/components/Card.js
+++ src/components/Card.js
@@ -3,7 +3,6 @@ import React from 'react';
 const Card = ({ name, email, id }) => {
   return (
     <div className="tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5">
-      {/* grow is the animation */}
       <img src={`https://robohash.org/${id}?200x200`} alt="robot" />
       <div>
         <h2>{name}</h2>
