e222050fcef25e199bde748004db7677bfbad7ab
StefaniaNeri <117984737+StefaniaNeri@users.noreply.github.com>
2023-04-06 20:49:38 +0200

    Update App.js

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import CardList from '../components/CardList';
 import SearchBox from '../components/SearchBox';
-//import { robots } from './robots'; // need to destructure it because it isn't default, it isn't only just one (well in this case is one, but it's not set to default so we need to tell it by destructuring)
 import Scroll from '../components/Scroll';
 import ErrorBoundry from '../components/ErrorBoundry';
 import './App.css';
@@ -17,8 +16,8 @@ class App extends Component {
 
   componentDidMount() {
     fetch('https://jsonplaceholder.typicode.com/users')
-      .then(response => response.json()) // we make an http request, then convert the response to json.
-      .then(users => this.setState({ robots: users })); // updating the users with setState
+      .then(response => response.json()) 
+      .then(users => this.setState({ robots: users })); 
   }
 
   onSearchChange = event => {
@@ -29,19 +28,15 @@ class App extends Component {
     const { robots, searchfield } = this.state;
     const filteredRobots = robots.filter(robot => {
       return robot.name.toLowerCase().includes(searchfield.toLowerCase());
-    }); // lowercase it's good for comparisons so we don't have to worry about upper or lowercase differences
+    }); 
     return !robots.length ? (
       <h1> Loading</h1>
     ) : (
       <div className="tc">
         <h1 className="f1">RoboFriends</h1>
         <SearchBox searchChange={this.onSearchChange} />{' '}
-        {/* passing this function as a prop */}
-        {/* 'this' is the App, which is an object */}
         <Scroll>
           <ErrorBoundry>
-            {' '}
-            {/*if anything in the CardList fails, it's going to catch it and display the error message*/}
             <CardList robots={filteredRobots} />
           </ErrorBoundry>
         </Scroll>
