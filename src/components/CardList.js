a17fee1912de89cfd866aa5635cb5f2ef78f6cc5
StefaniaNeri <117984737+StefaniaNeri@users.noreply.github.com>
2023-04-06 20:44:52 +0200

    Update CardList.js

--- src/components/CardList.js
+++ src/components/CardList.js
@@ -5,7 +5,6 @@ const CardList = ({ robots }) => {
   return (
     <div>
       {robots.map(user => {
-        // return <Card id={robots[i].id} name={robots[i].name} email={robots[i].email};
         return (
           <Card
             key={user.id}
@@ -13,7 +12,7 @@ const CardList = ({ robots }) => {
             name={user.name}
             email={user.email}
           />
-        ); /* the unique key will be the index of each iteration key={i}, but a key prop should have somethind that doesn't change, and index could change, so a better way would be something unique like the id*/
+        );
       })}
     </div>
   );
